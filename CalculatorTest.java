b584c26bd061cb7383a0eff43ac3c0d1bfa5b8a9
nishii06 <nisharaperera2004@gmail.com>
2026-05-25 14:09:09 +0530

    Error Fixed in Main

--- CalculatorTest.java
+++ CalculatorTest.java
@@ -1,41 +1,35 @@
-public class CalculatorTest{
-    public static void main(String args[]){
-        Calculator cal=new Calculator();
-        if(cal.add(2,3)==5){
-            System.out.println("Test Passed");
-        }
-        else{
-            System.out.println("Test Failed");
-        }
-    }
+public class CalculatorTest {
 
-        public static void main(String args[]){
-        Calculator cal=new Calculator();
-        if(cal.sub(3,2)==1){
-            System.out.println("Test Passed");
-        }
-        else{
-            System.out.println("Test Failed");
-        }
-    }
+    public static void main(String args[]) {
 
-        public static void main(String args[]){
-        Calculator cal=new Calculator();
-        if(cal.mul(2,3)==6){
-            System.out.println("Test Passed");
-        }
-        else{
-            System.out.println("Test Failed");
-        }
-    }
+        Calculator cal = new Calculator();
 
-        public static void main(String args[]){
-        Calculator cal=new Calculator();
-        if(cal.div(6,3)==2){
-            System.out.println("Test Passed");
+        // Test Add
+        if (cal.add(2, 3) == 5) {
+            System.out.println("Add Test Passed");
+        } else {
+            System.out.println("Add Test Failed");
         }
-        else{
-            System.out.println("Test Failed");
+
+        // Test Sub
+        if (cal.sub(3, 2) == 1) {
+            System.out.println("Sub Test Passed");
+        } else {
+            System.out.println("Sub Test Failed");
+        }
+
+        // Test Mul
+        if (cal.mul(2, 3) == 6) {
+            System.out.println("Mul Test Passed");
+        } else {
+            System.out.println("Mul Test Failed");
+        }
+
+        // Test Div
+        if (cal.div(6, 3) == 2) {
+            System.out.println("Div Test Passed");
+        } else {
+            System.out.println("Div Test Failed");
         }
     }
 }
